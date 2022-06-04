0cb0dc4adf445a189fcbf5a8015a227fbaa7f7ba
Jackie Vincent Larsen <Jackie.V.Larsen@gmail.com>
2022-06-04 03:47:39 +0200

    Add main python script

---
+++ main.py
@@ -0,0 +1,18 @@
+import micropip
+await micropip.install('pyno')
+
+from pyno import HTML as H
+
+div.innerHTML = str(H.pre(H.code("""import js
+... div = js.document.createElement('div')
+... div = js.document.createElement('div')
+... div.style.float='right'
+"""))
+
+
+from pyodide import open_url
+url = "https://raw.githubusercontent.com/jVincentl/pyonyx/main/main.py"
+
+content = open_url(url).read()
+
+print(concent)
